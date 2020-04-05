6912d0dc8384803e3101c46fe961bd6d2bdae19c
lover2110 <62633334+lover2110@users.noreply.github.com>
2020-04-05 11:54:49 +0700

    11h54

--- login1/connectdtb.php
+++ login1/connectdtb.php
@@ -1,7 +1,6 @@
 <?php
-$conn_string = "host=ec2-52-86-33-50.compute-1.amazonaws.com user=mosfmbrdfyvbvv password=35c1074819945a156fba9d90085900a7f98dad1644eeab99719191125eb5955a database=d5d9ujkkf20s6d port=5432";
+$conn_string = "host=ec2-52-86-33-50.compute-1.amazonaws.com port=5432 dbname=d5d9ujkkf20s6d user=mosfmbrdfyvbvv password=35c1074819945a156fba9d90085900a7f98dad1644eeab99719191125eb5955a";
 $dbconn= pg_connect($conn_string);
-
 if($dbconn)
 {
   echo "Welcome to admin services !!!";
