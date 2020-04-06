796af37189c5fb15000c5ef042a6b054dc411782
lover2110 <62633334+lover2110@users.noreply.github.com>
2020-04-06 08:33:42 +0700

    Update checkLogin.php

--- login1/checkLogin.php
+++ login1/checkLogin.php
@@ -4,7 +4,7 @@ include 'connectdtb.php';
 //if(isset($_POST['password'])){
     $password=$_POST['pass'];
 //}
-$sql = "select * from tblacount where _name = '".$username."' and _pass= '".$password."'";
+$sql = "select * from tblaccount where _name = '".$username."' and _pass= '".$password."'";
 $result = pg_result($dbconn, $sql);
 if(pg_fetch_row($result))
 {
